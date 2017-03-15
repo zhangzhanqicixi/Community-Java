f801c18366ca8d162c28caaa0eebbffd662339a6
zhangzhanqi <zhangzhanqicixi@gmail.com>
2017-03-16 00:12:00 +0800

    1703160011

--- src/main/java/me/cmnt/dao/impl/MemberDaoImpl.java
+++ src/main/java/me/cmnt/dao/impl/MemberDaoImpl.java
@@ -26,6 +26,7 @@ public class MemberDaoImpl extends BaseDaoImpl {
 				case 1: HQL = "from Member where id = " + member.getId(); break;
 				case 2: HQL = "from Member where user_id = " + member.getUser_id(); break; 
 				case 3: HQL = "from Member where community_id = " + member.getCommunity_id(); break;
+				case 4: HQL = "from Member where community_id = " + member.getCommunity_id() + "and member_type = " + member.getMember_type(); break;
 			}
 			if (!HQL.isEmpty()) {
 				return sessionFactory.getCurrentSession().createQuery(HQL).list();
